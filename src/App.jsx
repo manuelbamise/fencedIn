76a0743a6b1807b15d457edc430b6deeb89fd00a
Manuel <etobi1930@gmail.com>
2023-12-17 06:55:42 +0100

    Initial commit, started working on the outer layer of fencedIn

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Testimonials</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <header>
+        <h1>fencedIn</h1>
+        <p>Quality security fences for farmers</p>
+      </header>
+      <main>
+        <h2>About Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+          accusantium exercitationem tempore facilis! Mollitia magni corrupti,
+          expedita illo soluta commodi esse voluptatibus quidem sapiente qui
+          aut, nihil nemo placeat laborum.
+        </p>
+        <section>
+          <section>
+            <h4>Our Mission</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              fugiat voluptate, quos, quod dolor inventore autem mollitia quas
+              harum amet veniam velit impedit doloremque exercitationem fugit.
+              Saepe quam repudiandae omnis.
+            </p>
+          </section>
+          <section>
+            <h4>Our Vision</h4>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
+              incidunt quod nihil cum natus totam, saepe, quo autem distinctio
+              animi architecto, quasi officia harum soluta doloribus alias illo
+              beatae eaque.
+            </p>
+          </section>
+        </section>
+        <section>
+          <h3>Why choose us?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+            accusamus eligendi at nisi reiciendis vero vitae iure maxime quaerat
+            quisquam nesciunt assumenda, impedit a voluptates aliquam! Quae
+            fugiat at corporis!
+          </p>
+        </section>
+        <section>
+          <h3>Testimonials</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Dignissimos molestiae eum voluptatibus ex accusantium provident
+            facere ad culpa, dolores deleniti nostrum ipsum rem, omnis eveniet
+            consequuntur natus repellat vitae perferendis. Eius, quis laboriosam
+            asperiores nostrum perspiciatis itaque porro fuga saepe ab qui
+            quidem, harum, minima officiis? Temporibus unde aut culpa provident,
+            at pariatur est eius, praesentium dicta iusto enim ipsa.
+          </p>
+        </section>
+        <section>
+          <h3>Q/A</h3>
+          <ul>
+            <li>
+              qusetion <br />
+              <i>answer</i>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        <h2>fencedIn</h2>
+        {/* Continue with the designs fo fencedIn */}
+      </footer>
+    </>
+  );
+};
+
+export default App;
