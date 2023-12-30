991c404da9f15e5fce61ee53a4ebe533ec53e174
manuelbamise <etobi1930@gmail.com>
2023-12-30 22:30:56 +0100

    Trying to figure out how to add event listeners on react

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fencedIn
+
+fenced in is a site for a company meant to supply fences for `farmers`
--- src/components/QuestionRow.jsx
+++ src/components/QuestionRow.jsx
@@ -1,7 +1,8 @@
 const QuestionRow = () => {
+  
   return (
-    <li className="border border-green-600 w-[60%]">
-      <div className="question">how long does shipping take? <span>&dArr;</span></div>
+    <li className="border border-green-600 w-[60%] text-center">
+      <div className="question">how long does shipping take? <span id="dropDown">&dArr;</span></div>
       <div className="answer">3 to 4 weeks in US approved states</div>
     </li>
   );
