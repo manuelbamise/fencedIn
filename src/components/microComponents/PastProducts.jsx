274c3719eeaba3f8b3661d234683f021e2ce99d3
manuelbamise <etobi1930@gmail.com>
2024-02-03 23:18:18 +0100

    Thing to note already commented in the pastProducts.module.css file about how to make the elements stack

--- src/components/microComponents/PastProducts.jsx
+++ src/components/microComponents/PastProducts.jsx
@@ -1,14 +1,24 @@
 // import { useState } from "react";
-// import style from "../../assets/css/PastProducts.module.css";
+import style from "../../assets/css/PastProducts.module.css";
 import productRecords from "../../assets/json/pastProducts.json";
 
 const PastProducts = () => {
   return (
-    <div>jdfdjf
-      {productRecords &&
-        productRecords.map((record) =>{return(
-          <img src={record.imgLink} alt="" key={record.id}/>
-        )})}
+    <div className={style.container}>
+      jdfdjf
+      <div className={style.productContainer}>
+        {productRecords &&
+          productRecords.map((record) => {
+            return (
+              <img
+                src={record.imgLink}
+                alt="image"
+                key={record.id}
+                className={style.product}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
