e4118845828704219f5a9a9e5ad495299f7bf389
manuelbamise <etobi1930@gmail.com>
2024-02-10 09:35:26 +0100

    having little problems, trying to figure out how to map specific data from the json file to the frontend at the click of a button

--- src/components/microComponents/PastProducts.jsx
+++ src/components/microComponents/PastProducts.jsx
@@ -1,11 +1,40 @@
 // import { useState } from "react";
+import { useState } from "react";
 import style from "../../assets/css/PastProducts.module.css";
 import productRecords from "../../assets/json/pastProducts.json";
 
 const PastProducts = () => {
+  const [toggleType, setToggletype] = useState([
+    {
+      id: 1,
+      name: "Budget",
+      state: true,
+    },
+    {
+      id: 2,
+      name: "Midrange",
+      state: false,
+    },
+    {
+      id: 3,
+      name: "Flagship",
+      state: false,
+    },
+  ]);
+
+ 
   return (
-    <div className={style.container}>
-      jdfdjf
+    <div className=" border-2 border-black h-96 w-[80rem] m-auto">
+      <div>
+        {toggleType &&
+          toggleType.map((btn) => {
+            return (
+              <button key={btn.id}>
+                {btn.name}
+              </button>
+            );
+          })}
+      </div>
       <div className={style.productContainer}>
         {productRecords &&
           productRecords.map((record) => {
