a0c1c342d82dcc54750e83c6829c867c551c4be2
manuelbamise <etobi1930@gmail.com>
2024-03-01 13:38:09 +0100

    Another commit

--- src/components/microComponents/PastProducts.jsx
+++ src/components/microComponents/PastProducts.jsx
@@ -22,19 +22,19 @@ const PastProducts = () => {
     },
   ];
 
-  const [activebtn, setactivebtn] = useState("budget");//sets the default button to budget
+  const [activebtn, setactivebtn] = useState("budget"); //sets the default button to budget
   const [filteredItems, setFilteredItems] = useState([]);
 
   useEffect(() => {
     // the code in this function filters through a list of data to be rendered on the page
     // as soon as the state of the activebtn changes
     const filterItems = () => {
-      const filteredResults = productRecords.filter(//filters through the data and stores it in a new variable
+      const filteredResults = productRecords.filter(
+        //filters through the data and stores it in a new variable
         (item) => item.modelType === activebtn
       );
       setFilteredItems(filteredResults);
     };
-    
 
     filterItems();
   }, [activebtn]);
@@ -48,15 +48,20 @@ const PastProducts = () => {
     <div
       className=" border-2 border-black h-96 w-[80rem] m-auto"
       id="productContainer"
-    > 
+    >
       <div className="border flex justify-between w-optimal p-3 m-auto">
         {toggleBtn &&
           toggleBtn.map((item) => {
-            return (//renders button onto the page
+            return (
+              //renders button onto the page
               <button
                 onClick={() => handleButtonClick(item.modelType)}
                 key={item.id}
-                className={activebtn === item.modelType ? 'ring-2 ring-stapleGreen p-2 rounded ' : 'bg-black font-bold p-2 rounded text-white hover:bg-stapleGreen hover:p-2 hover:rounded'}
+                className={
+                  activebtn === item.modelType
+                    ? "ring-2 ring-stapleGreen p-2 rounded "
+                    : "bg-black font-bold p-2 rounded text-white hover:bg-stapleGreen hover:p-2 hover:rounded"
+                }
               >
                 {item.Name}
               </button>
@@ -66,7 +71,8 @@ const PastProducts = () => {
       <div className="m-auto w-[80%] flex justify-center border-4 border-black">
         {filteredItems &&
           filteredItems.map((item) => {
-            return (//renders filtered data onto the page
+            return (
+              //renders filtered data onto the page
               <img
                 src={item.imgLink}
                 alt={item.modelType}
