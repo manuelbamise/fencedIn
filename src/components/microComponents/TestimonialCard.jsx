fcd5131173ea65518fa5d17310395fd5fdf1243c
manuelbamise <etobi1930@gmail.com>
2024-03-07 20:58:04 +0100

    Need two buttons to cycle through the testimonials,

--- src/components/microComponents/TestimonialCard.jsx
+++ src/components/microComponents/TestimonialCard.jsx
@@ -1,27 +1,53 @@
+import { useEffect, useState } from "react";
 import testimonial from "../../assets/json/testimonials.json";
 
 const TestimonialCard = () => {
+  const [activeTestimonial, setactiveTestimonial] = useState("1");
+  const [filterdata, setFilteredata] = useState([]);
+  useEffect(() => {
+    const filterItems = () => {
+      const filteredResults = testimonial.filter(
+        //filters through the data and stores it in a new variable
+        (item) => item.id === activeTestimonial
+      );
+      setFilteredata(filteredResults);
+    };
+
+    filterItems();
+  }, [activeTestimonial]);
+
+  const handleButtonClick = (buttontype) => {
+    //sets parameter(buttontype) as activebtn
+    setactiveTestimonial(buttontype);
+  };
+
   return (
     <div className="testimonialCard">
-      {testimonial && testimonial 
-      ? testimonial.map((item)=>{
-        return(
-          <div key={item.id}>
-            <div>
-              <div>{item.firstName}</div>
-              <div>{item.lastName}</div>
-            </div>
-            <div>
-              {item.testimonial}
-            </div>
-            <div>
-              <a href={item.linkedinUrl}>Linkedin</a>
-              <a href={item.twitterUrl}>Twitter</a>
-              <a href={item.instagramUrl}>Instagram</a>
-            </div>
-          </div>
-        ) 
-      }) : "No records found"}
+      {testimonial && testimonial
+        ? testimonial.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className=" border border-red-700 text-center my-2 w-[250px] relative"
+              >
+                <button onClick={()=> handleButtonClick(`${item.id + 1}`)}>Left</button>
+                <div>
+                <div>
+                  <div>{item.firstName}</div>
+                  <div>{item.lastName}</div>
+                </div>
+                <div>{item.testimonial}</div>
+                <div>
+                  <a href={item.linkedinUrl}>Linkedin</a>
+                  <a href={item.twitterUrl}>Twitter</a>
+                  <a href={item.instagramUrl}>Instagram</a>
+                </div>
+                <button>right</button>
+                </div>
+              </div>
+            );
+          })
+        : "No records found"}
     </div>
   );
 };
